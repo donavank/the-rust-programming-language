843d919682f4bd8b0011efc1df541b69c8292c98
donavank <donavankeen97@gmail.com>
2023-11-18 16:08:01 -0500

    Completed Chapter 2
    * Added finished main.rs
    
    * Added some additional input checking to allow the user to 'exit'

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -1,20 +1,46 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+    println!("Enter 'exit' to quit.");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess = guess.trim();
 
-    println!("You guessed: {guess}");
+        match guess.trim().to_lowercase().cmp(&String::from("exit")) {
+            Ordering::Equal => {
+                println!("Exiting...");
+                return;
+            }
+            Ordering::Greater | Ordering::Less => println!("You guessed: {guess}"),
+        };
+
+        let guess: u32 = match guess.parse() {
+            // match parse result to assign guess
+            Ok(num) => num,     // user entered a number
+            Err(_) => continue, // user did not enter a number
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"), // guess < secret_number
+            Ordering::Greater => println!("Too big!"), // guess > secret_number
+            Ordering::Equal => {
+                // guess == secret_number
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
