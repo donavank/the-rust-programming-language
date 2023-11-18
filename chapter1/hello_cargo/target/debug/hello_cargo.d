f675bf19b574fcb0d3b8d01b7b37294d248a0165
donavank <donavankeen97@gmail.com>
2023-11-18 14:34:00 -0500

    * Started Chapter 1 and added hello world and hello cargo projects

---
+++ chapter1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\dev\code\the-rust-programming-language\chapter1\hello_cargo\target\debug\hello_cargo.exe: C:\dev\code\the-rust-programming-language\chapter1\hello_cargo\src\main.rs
---
+++ chapter1/hello_world/hello_world.rs
@@ -0,0 +1,3 @@
+fn main() {
+    lprintln!("Hello, world!");
+}
